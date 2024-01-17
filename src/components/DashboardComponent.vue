c543bd56b11bd90ceb565c124e3ae9e55a4040ac
Shugmi Shumunov <SShumunov@gmail.com>
2024-01-16 23:21:31 -0500

    feat: Create a pinia store for the dashboard data
    - pull in data from pinia

--- src/components/DashboardComponent.vue
+++ src/components/DashboardComponent.vue
@@ -8,8 +8,8 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
-import { DashboardData } from './models';
+import { defineComponent } from 'vue';
+import { useDashboardStore } from '../stores/dashboard';
 // Apex Charts
 import ApexCharts from './Charts/Apex/ApexCharts.vue';
 // Charts.js Charts
@@ -24,43 +24,8 @@ export default defineComponent({
     DashboardCard,
   },
   setup() {
-    const data = ref<DashboardData[]>([
-      {
-        title: 'Create Layout',
-        description: 'Create a layout for the dashboard',
-        completed: true,
-      },
-      {
-        title: 'Add Graphs and Charts',
-        description:
-          'Add graphs and charts to the dashboard that display data and can be interacted with',
-        completed: 'in-progress',
-        notes: 'More to come, add D3 charts in the future',
-      },
-      {
-        title: 'Expand Dashboard',
-        description: 'Expand the dashboard to include more data',
-        completed: false,
-      },
-      {
-        title: 'Improve Dashboard UI',
-        description: 'Improve the dashboard UI and make more robust',
-        completed: false,
-      },
-      {
-        title: 'Connect to external data',
-        description:
-          'Connect to an external data source and display data in the dashboard, like an API or store',
-        completed: null,
-      },
-      {
-        title: 'Allow user to mark tasks as complete',
-        description:
-          'Allow the user to mark tasks as complete and save the state of the tasks, update UI to reflect this',
-        completed: null,
-      },
-    ]);
-    return { data };
+    const store = useDashboardStore();
+    return { data: store.$state.data };
   },
 });
 </script>
