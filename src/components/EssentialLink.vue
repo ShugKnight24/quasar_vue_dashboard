1a967a07fcfe3b0f2fa9b141b1fa16afaeebbaa3
Shugmi Shumunov <SShumunov@gmail.com>
2024-03-13 17:23:32 -0400

    refactor: `EssentialLink`s uses setup script
    - Create props and pass in defaults

--- src/components/EssentialLink.vue
+++ src/components/EssentialLink.vue
@@ -1,14 +1,9 @@
+<!-- Connect to the router and have different pages..? -->
+<!-- Create a UI that allows you to move from the charts / graphs / to CRUD Table... -->
+<!-- Look at the old PHP CRUD app you made a long time ago -->
 <template>
-  <q-item
-    clickable
-    tag="a"
-    target="_blank"
-    :href="link"
-  >
-    <q-item-section
-      v-if="icon"
-      avatar
-    >
+  <q-item clickable tag="a" target="_blank" :href="link">
+    <q-item-section v-if="icon" avatar>
       <q-icon :name="icon" />
     </q-item-section>
 
@@ -18,32 +13,20 @@
     </q-item-section>
   </q-item>
 </template>
+<script setup lang="ts">
+import { withDefaults } from 'vue';
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+interface EssentialLinkProps {
+  title: string;
+  caption?: string;
+  link?: string;
+  icon?: string;
+}
 
-export default defineComponent({
-  name: 'EssentialLink',
-  props: {
-    title: {
-      type: String,
-      required: true
-    },
-
-    caption: {
-      type: String,
-      default: ''
-    },
-
-    link: {
-      type: String,
-      default: '#'
-    },
-
-    icon: {
-      type: String,
-      default: ''
-    }
-  }
+withDefaults(defineProps<EssentialLinkProps>(), {
+  title: '',
+  caption: '',
+  link: '#',
+  icon: '',
 });
 </script>
