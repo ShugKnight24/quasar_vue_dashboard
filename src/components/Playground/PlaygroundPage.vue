bb94d99c40a7318de196d24f0f91ba682caa944c
Shugmi Shumunov <SShumunov@gmail.com>
2024-04-25 15:16:10 -0400

    feat: Add page to test `Object.defineProperties`

--- src/components/Playground/PlaygroundPage.vue
+++ src/components/Playground/PlaygroundPage.vue
@@ -1,3 +1,10 @@
 <template>
-  <h1>Playground</h1>
+  <div class="q-pa-md">
+    <h1 class="q-my-sm">Playground</h1>
+    <p>Playground for testing out Vue 3 features</p>
+    <ObjectTest />
+  </div>
 </template>
+<script setup lang="ts">
+import ObjectTest from './../ObjectTest.vue';
+</script>
