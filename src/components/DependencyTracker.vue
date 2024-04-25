089900645a360b84c1039525ee1b5055d690e4cb
Shugmi Shumunov <SShumunov@gmail.com>
2024-04-25 16:38:44 -0400

    feat: Add component to play with dependency subscriptions

---
+++ src/components/DependencyTracker.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="q-pa-md">
+    <h1 class="q-my-sm">Dependency Tracker</h1>
+    <p>Dependency tracker for Vue 3</p>
+    <p>
+      There will be a maximum call stack error due to the recursive nature of
+      this function
+    </p>
+    <p>TODO: Add an interval or timeout to exit before that occurs</p>
+  </div>
+</template>
+<script setup lang="ts">
+// Avoid using the Function type
+type UpdateFunction = () => void;
+
+class DependencyTracker {
+  dependents: Set<UpdateFunction>;
+
+  constructor() {
+    this.dependents = new Set(); // Prevent duplicates
+  }
+
+  depend() {
+    if (currentUpdate) {
+      this.dependents.add(currentUpdate);
+      console.log('Dependency has been tracked');
+    }
+  }
+
+  notify() {
+    this.dependents.forEach((effect: UpdateFunction) => {
+      effect();
+      console.log('Effect has been triggered');
+    });
+  }
+}
+
+const currentDependency = new DependencyTracker();
+
+let currentUpdate: UpdateFunction | null;
+
+function autorun(update: UpdateFunction) {
+  function wrappedUpdate() {
+    currentUpdate = wrappedUpdate;
+    update();
+    currentUpdate = null;
+  }
+  wrappedUpdate();
+}
+
+autorun(() => {
+  currentDependency.depend();
+  currentDependency.notify();
+});
+</script>
--- src/components/Playground/PlaygroundPage.vue
+++ src/components/Playground/PlaygroundPage.vue
@@ -2,9 +2,11 @@
   <div class="q-pa-md">
     <h1 class="q-my-sm">Playground</h1>
     <p>Playground for testing out Vue 3 features</p>
-    <ObjectTest />
+    <!-- <ObjectTest /> -->
+    <DependencyTracker />
   </div>
 </template>
 <script setup lang="ts">
-import ObjectTest from './../ObjectTest.vue';
+import DependencyTracker from './../DependencyTracker.vue';
+// import ObjectTest from './../ObjectTest.vue';
 </script>
