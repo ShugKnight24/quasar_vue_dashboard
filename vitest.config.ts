201ff054dc885f2b6ba62d7d4886e7dfa56704ad
Shugmi Shumunov <SShumunov@gmail.com>
2024-04-26 19:38:30 -0400

    chore: Vitest ignores Jest tests
    - Update to fix for experienced errors

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,10 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true, // Enable Jest-like global APIs
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/test/jest/**', // Exclude the test/jest directory
+    ],
   },
 });
