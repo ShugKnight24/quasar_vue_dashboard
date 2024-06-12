c37684ae97a7f9c8aad1ba4ee6e0a93c0a05b6ff
Shugmi Shumunov <SShumunov@gmail.com>
2024-06-12 16:24:22 -0400

    refactor: Add validation to the ColorsDataGrid
    - Add # after the fact and add a regex to ensure hexcode only contains 
    `A-F`, `0-9`, and is either 3 or 6 characters long

--- src/components/AgGrid/ColorsDataGrid.vue
+++ src/components/AgGrid/ColorsDataGrid.vue
@@ -10,6 +10,7 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import { AgGridVue } from 'ag-grid-vue3';
+import { ValueFormatterParams, ValueParserParams } from 'ag-grid-community';
 
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-material.css';
@@ -39,19 +40,36 @@ const columns = ref([
     editable: true,
     cellEditor: 'agTextCellEditor',
     cellEditorParams: {
-      maxLength: 7,
-      minLength: 4,
+      maxLength: 6,
+      minLength: 3,
     },
-    // TODO: Add validation to ensure there is a # at the beginning of the hex code
-    // TODO: Figure out how you can add a Select for the color picker that updates the hex code on change
+    valueFormatter: (params: ValueFormatterParams) => {
+      let hexCode = params.value;
+      hexCode = hexCode.charAt(0) === '#' ? hexCode : '#' + hexCode;
+      return hexCode.toUpperCase();
+    },
+    // Don't include # in the data, format it on change
+    valueParser: (params: ValueParserParams) => {
+      let hexCode = params.newValue;
+      if (hexCode.startsWith('#')) {
+        hexCode = hexCode.slice(1);
+      }
+      const hexCodeRegex = /^([a-fA-F0-9]{3}|[a-fA-F0-9]{6})$/i;
+      if (!hexCodeRegex.test(hexCode)) {
+        // TODO: Figure out how to display an intuitive error message that's not an alert
+        return params.oldValue;
+      }
+      return hexCode;
+    },
   },
 ]);
 
+// TODO: Figure out how you can add a Select for the color picker that updates the hex code on change
 const rowData = ref([
-  { color: 'Red', description: 'The color of blood', hexCode: '#FF0000' },
-  { color: 'Orange', description: 'The color of oranges', hexCode: '#FFA500' },
-  { color: 'Yellow', description: 'The color of the sun', hexCode: '#FFFF00' },
-  { color: 'Green', description: 'The color of grass', hexCode: '#008000' },
-  { color: 'Blue', description: 'The color of the sky', hexCode: '#0000FF' },
+  { color: 'Red', description: 'The color of blood', hexCode: 'FF0000' },
+  { color: 'Orange', description: 'The color of oranges', hexCode: 'FFA500' },
+  { color: 'Yellow', description: 'The color of the sun', hexCode: 'FFFF00' },
+  { color: 'Green', description: 'The color of grass', hexCode: '008000' },
+  { color: 'Blue', description: 'The color of the sky', hexCode: '0000FF' },
 ]);
 </script>
